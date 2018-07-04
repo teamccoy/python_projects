6b6290d87ff1b596e59f728ae4b3e38bd9c1203f
teamccoy <37424882+teamccoy@users.noreply.github.com>
2018-07-04 08:25:58 -0700

    Update rock_paper_scissor_game.py

--- rock_paper_scissor_game.py
+++ rock_paper_scissor_game.py
@@ -4,11 +4,12 @@ options = ["r", "p", "s"]
 user_play = "y"
 # using random choice to iterate through list
 computer_choice = random.choice(options)
-#Ask the user for information and specify how input should be given.
+
 
 
 #set up a while loop to continue to play after each game. While the input is "y" the game will ask for a choice and the IF statement will execute
 while user_play == "y":
+	#Ask the user for information and specify how input should be given.
 	user_choice = input("May your choice: (r)ock, (p)aper, (s)cissors ?")
 #use if conditionals to determine which outcome is true to determine winner
 	if(user_choice =="r" and computer_choice == "p"):
